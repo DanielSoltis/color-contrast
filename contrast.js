5b963af69180ffa06610a0122fc0089fc330f0d0
DanielSoltis <danielrsoltis@gmail.com>
2022-08-23 16:00:09 -0400

    fix uppercase bug
    fix so that uppercase and lowercase letters are accepted in hex values

--- contrast.js
+++ contrast.js
@@ -5,7 +5,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OiR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 //DATA STRUCTURE
@@ -789,7 +789,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     };
     
     isValidColor = function (color) {
-        var matchHexAndRgb = (color.hex === rgbToHex(color.rgb));
+        var matchHexAndRgb = (color.hex.toLowerCase() === rgbToHex(color.rgb));
         return isValidRgb(color.rgb) && matchHexAndRgb;
     };
     
@@ -818,7 +818,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
     //takes a contrast value (0-21) and returns a text string corresponding to WCAG score
     interpretContrast = function (ct) {
-        if (ct < 3.1) {
+        if (ct < 3) {
             return "Fail";
         }
         if (ct < 4.5) {
